530d9371b95add12022d904ab2f38f7cf7c88744
Aditya Dhanraj <opsingh861@gmail.com>
2024-07-16 16:54:30 +0530

    playlist

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using graphQL.Types;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>()
+   .AddType<Playlist>();
+
+builder
+  .Services
+  .AddCors(options =>
+  {
+      options.AddDefaultPolicy(builder =>
+      {
+          builder
+              .WithOrigins("https://studio.apollographql.com")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+      });
+  });
+
+var app = builder.Build();
+
+app.UseCors();
+
+app.MapGraphQL();
+
+app.Run();
---
+++ Types/Query.cs
@@ -0,0 +1,26 @@
+﻿namespace graphQL.Types;
+
+public class Query
+{
+    public string Hello()
+    {
+        return "Hello world";
+    }
+
+    [GraphQLDescription("Playlists hand-picked to be featured to all users.")]
+    public List<Playlist> FeaturedPlaylists()
+    {
+        //     return new List<Playlist> // we can write or use short hand which is below
+        //   {
+        //       new("1", "GraphQL Groovin'"),
+        //       new("2", "Graph Explorer Jams"),
+        //       new("3", "Interpretive GraphQL Dance")
+        //   };
+        return
+      [
+          new("1", "GraphQL Groovin"),
+          new("2", "Graph Explorer Jams"),
+          new("3", "Interpretive GraphQL Dance")
+      ];
+    }
+}
